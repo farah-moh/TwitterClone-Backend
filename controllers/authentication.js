8fc401aa0d27887333d6f37f18f01d74f7db97ec
farah-moh <75073858+farah-moh@users.noreply.github.com>
2022-04-19 22:43:34 +0200

    Password Reset & Change - Untested

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -6,13 +6,17 @@ const AppError = require('../utils/appError');
 const jwt = require('jsonwebtoken');
 const { promisify } = require('util');
 const sendEmail = require('./../utils/email_info');
+const user = require('../models/user');
 
 //Sign Up Services + Route Handlers
 
+
+//   Creating Sign Token
 const signToken = id => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRE })
 }
 
+//   Creating new user
 const createUser = async body => {
     const newUser = await user.create({
         username: body.username,
@@ -26,6 +30,9 @@ const createUser = async body => {
       return newUser;
 }
 exports.createUser = createUser;
+
+
+/*  ******* Sign Up + Confirmation ******* */
 
 exports.signUp = catchAsync(async (req, res, next) => {
     const newUser = await createUser(req.body); 
@@ -48,7 +55,7 @@ exports.signUp = catchAsync(async (req, res, next) => {
       });
     } catch (err) {
       newUser.confirmEmailToken = undefined;
-      newUser.passwordResetTokenExpiry = undefined;
+      newUser.confirmEmailTokenExpiry = undefined;
       await newUser.save({
         validateBeforeSave: false
       });
@@ -92,8 +99,7 @@ exports.signUpConfirmed = catchAsync(async (req, res, next) => {
 })
 
 
-/* ------------------------------------------------------------------------- */
-
+/*  ******* Login + Validation + Facebook Login ******* */
 
 const validateLogin = async (email, password) => {
     const User = await user.findOne({email: email});
@@ -131,7 +137,7 @@ exports.login = catchAsync(async (req, res, next) => {
     })
   });
 
-  exports.loginWithFacebook = catchAsync(async (req, res, next) => {
+exports.loginWithFacebook = catchAsync(async (req, res, next) => {
     const token = signToken(req.user_id);
 
     res.status(200).json({
@@ -141,3 +147,88 @@ exports.login = catchAsync(async (req, res, next) => {
       token
       })
   })
+
+
+/*  ******* Forgot Password + Change Password ******* */
+
+  //email is sent in body
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+    await sendForgotPasswordToken(req.body.email);
+  
+    res.status(200).json({
+      status: 'success',
+      message: 'Token sent to email!'
+    });
+  });
+
+
+const sendForgotPasswordToken = async email => {
+    const User = await user.findOne(email);
+    if (!User) {
+      throw new AppError('Email not found.', 404);
+    }
+    const resetToken = await user.generatePasswordResetToken();
+    const resetPasswordText = 
+    `Click this link to reset your password\n
+    http://localhost:${process.env.PORT}/password-reset/${emailConfirmationToken}\n`;
+
+    try {
+      await sendEmail({
+        email: User.email,
+        subject: 'Password Reset Token',
+        message: resetPasswordText
+      });
+      res.status(200).json({
+        status: 'success',
+        message: 'Check your email for password reset.'
+      });
+    } catch (err) {
+      newUser.passwordResetToken = undefined;
+      newUser.passwordResetTokenExpiry = undefined;
+      await newUser.save({
+        validateBeforeSave: false
+      });
+      throw new AppError(
+        `There was an error sending the email. ${err} `, 500
+      );
+    }
+  };
+  exports.sendForgotPasswordToken = sendForgotPasswordToken;
+
+const resetPassword = async (token, password) => {
+    const hashedToken = crypto
+      .createHash('SHA256')
+      .update(token)
+      .digest('hex');
+  
+    const User = await user.findOne(
+      {
+        passwordResetToken: hashedToken,
+        passwordResetExpiresAt: {$gt: Date.now()}
+      }
+    );
+  
+    if (!user) throw new AppError(`Token is invalid or has expired`, 400);
+  
+    user.password = password;
+    user.passwordResetToken = undefined;
+    user.passwordResetTokenExpiry = undefined;
+    await user.save();
+  
+    return user;
+  };
+exports.resetPasswordService = resetPassword;
+
+const changePassword = async (id, password, newPassword) => {
+  const User = await user.findOne({_id: id});
+
+  if (!User) throw new AppError('Invalid User', 400);
+
+  if (!(await user.validatePassword(password, user.password)))
+    throw new AppError('Incorrect password', 401);
+
+  User.password = newPassword;
+  await User.save();
+  return User;
+};
+exports.changePassword = changePassword;
