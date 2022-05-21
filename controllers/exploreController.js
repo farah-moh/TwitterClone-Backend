7a8c475d7ab594a170d607f371853d76ff22cd42
Mohamed Mostafa <mohamedmostafa1632001@gmail.com>
2022-05-21 23:06:34 +0200

    Updated trending tweets

--- controllers/exploreController.js
+++ controllers/exploreController.js
@@ -42,7 +42,7 @@ const getTrending= async() => {
     const tweetsCount=[];
     for (let i=0;i<result2.length;i++)
     {
-        const tweets= await getAllTweets(result2[i]);
+        const tweets= await getAllTweets(result2[i],0);
         tweetsCount.push(tweets.length);
     }
     const data = await result2.map(function(bdy,i)
@@ -59,13 +59,131 @@ const getTrending= async() => {
 }
 exports.getTrending = getTrending;
 
-const getAllTweets= async(sentHashtag) => {
+const getAllTweets= async(sentHashtag,mode) => {
     const tweets = await tweet.find({hashtags: sentHashtag});
+    if(mode===0)
+    {
+        return tweets;
+    }
     const tweets1 = await _.sortBy(tweets,[function(o) {
         return o.retweeters.length;
     }]);
     const tweets2 = await tweets1.reverse();
-    return tweets2;
+    const data=[];
+    for (let i=0;i<tweets2.length;i++)
+    {
+        const User = await user.findById(ObjectId(tweets2[i].user));
+        let isLikedNew = false;
+                    if(User.likedTweets){
+                        for(let likedTweet of User.likedTweets){
+                            if((likedTweet).toString() === (tweets2[i]._id).toString())
+                                isLikedNew = true
+                        }
+                    }
+                    let isLiked = (isLikedNew)? "true" : "false";
+                    //Check if its retweeted by the main user
+             
+                    let isRetweetedNew = false;
+                    if(User.retweetedTweets){
+                        for(let retweetedTweet of User.retweetedTweets){
+                            if((retweetedTweet).toString() === (tweets2[i]._id).toString())
+                                isRetweetedNew = true
+                        }
+                    }
+                
+                    let isRetweeted = (!isRetweetedNew)? "false" : "true";
+                    //Check if it's bookmarked by the mainUser
+                    let isBookmarked = false;
+                    
+                    if(User.bookMarkedTweets){
+                        for(let tweetBookmarked of User.bookMarkedTweets){
+                            if((tweetBookmarked).toString() === (tweets2[i]._id).toString())
+                                isBookmarked = true
+                        }
+                    }
+                    
+
+                    //Checking if there is a pooll
+                    let poll = (tweets2[i].poll)? "true" : "false";
+                    let firstChoice = "";
+                    let firstChoiceStats = 0;
+                    let secondChoice = "";
+                    let secondChoiceStats = 0;
+                    let thirdChoice = "";
+                    let thirdChoiceStats = 0;
+                    let fourthChoice = "";
+                    let fourthChoiceStats = 0;
+                    //Calculating the stats
+                    if(tweets2[i].poll){
+                        let sum = 0;
+                        let newPoll = await polls.findById(tweets2[i].poll)
+                        if(newPoll.choice1){
+                            sum += newPoll.choice1Statistics.length;
+                            firstChoice = newPoll.choice1;
+                        }
+                        if(newPoll.choice2){
+                            sum += newPoll.choice2Statistics.length;
+                            secondChoice =newPoll.choice2;
+                        }
+                        if(newPoll.choice3){
+                            sum += newPoll.choice3Statistics.length;
+                            thirdChoice = newPoll.choice3;
+                        }
+                        if(newPoll.choice4){
+                            sum +=newPoll.choice4Statistics.length;
+                            fourthChoice = newPoll.choice4;
+                        }
+                        if(newPoll.choice1 && sum!=0)
+                            firstChoiceStats = (newPoll.choice1Statistics.length)/sum
+                        if(newPoll.choice2 && sum!=0)
+                            secondChoiceStats = (newPoll.choice2Statistics.length)/sum
+                        if(newPoll.choice3 && sum!=0)
+                            thirdChoiceStats = (newPoll.choice3Statistics.length)/sum
+                        if(newPoll.choice4 && sum!=0)
+                            fourthChoiceStats = (newPoll.choice4Statistics.length)/sum
+
+                    }
+                    // writing the json file hat will be sent
+                    let usernamesTagged = []
+                    if(tweets2[i].taggedUsers.length){
+                        for(let index of tweets2[i].taggedUsers){
+                            let userTagged = await user.findById(index)
+                            if(userTagged){
+                                usernamesTagged.push(userTagged.username);
+                            }
+                        }
+                    } 
+        const info = {
+                        key:tweets2[i]._id,
+                        username: User.username,
+                        name: User.name,
+                        email: User.email,
+                        userImage: User.image,
+                        tweetBody: tweets2[i].body,
+                        tweetMedia: tweets2[i].media,
+                        repliesCount: (tweets2[i].replies)? (tweets2[i].replies).length: 0, 
+                        retweetersCount: (tweets2[i].retweeters)?(tweets2[i].retweeters).length : 0,
+                        favoritersCount: (tweets2[i].favoriters)? (tweets2[i].favoriters).length : 0,
+                        createdAt: tweets2[i].createdAt,
+                        taggedUsers: usernamesTagged,
+                        isLikedByUser: isLiked,
+                        isRetweetedByUser: isRetweeted,
+                        isBookmarkedByUser: isBookmarked,
+                        isAdmin: User.isAdmin,
+                        poll: poll,
+                        firstChoice: firstChoice,
+                        firstChoiceStats: firstChoiceStats*100,
+                        secondChoice: secondChoice,
+                        secondChoiceStats: secondChoiceStats*100,
+                        thirdChoice: thirdChoice,
+                        thirdChoiceStats: thirdChoiceStats*100,
+                        fourthChoice: fourthChoice,
+                        fourthChoiceStats: fourthChoiceStats*100,
+                        hashtags: tweets2[i].hashtags
+        }
+        data.push(info);
+    }
+    return data;
 }
 exports.getAllTweets = getAllTweets;
 
